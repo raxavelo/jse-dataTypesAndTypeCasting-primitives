a15a492aead703e4007cadfd166090d8ca0a8a1d
raxavelo <raxavelo@gmail.com>
2024-09-13 17:07:36 +0200

    Solved Question 1

---
+++ variables.js
@@ -0,0 +1,15 @@
+// Question 1: Write a code that will create variables and initialize them with values of Boolean, Number, BigInt, String, and undefined types using (when possible) literals and constructor functions.
+
+let b1 = true;
+let b2 = Boolean(false);
+
+let n1 = 23;
+let n2 = Number('3.14');
+
+let bi1 = 43n;
+let bi2 = BigInt('98765432109876543210');
+
+let s1 = 'Hello, World!';
+let s2 = String('123');
+
+let un = undefined;
