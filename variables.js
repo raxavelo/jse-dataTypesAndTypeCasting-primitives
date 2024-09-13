612854ddc100d29ccc96904b4133654fcafc4372
raxavelo <raxavelo@gmail.com>
2024-09-13 17:13:41 +0200

    Solved Question 2

--- variables.js
+++ variables.js
@@ -13,3 +13,19 @@ let s1 = 'Hello, World!';
 let s2 = String('123');
 
 let un = undefined;
+
+// Question 2: Print all values and all types of those values using console.log. Try to use string interpolation to display the value and type at the same time with a single console.log call, e.g. in the following form: 1000 [number].
+
+console.log(`${b1} [${typeof b1}]`);
+console.log(`${b2} [${typeof b2}]`);
+
+console.log(`${n1} [${typeof n1}]`);
+console.log(`${n2} [${typeof n2}]`);
+
+console.log(`${bi1} [${typeof bi1}]`);
+console.log(`${bi2} [${typeof bi2}]`);
+
+console.log(`${s1} [${typeof s1}]`);
+console.log(`${s2} [${typeof s2}]`);
+
+console.log(`${un} [${typeof un}]`);
